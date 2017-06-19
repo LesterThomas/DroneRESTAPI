eaf3af0b6bdac76eaf9a6283da75f4e21a8d6719
Lester Thomas <lesterthomas@hotmail.com>
2017-06-19 17:13:23 +0100

    Updated and simplified DOCKERFILE for droneapi server

--- buildAPIDocker.bash
+++ buildAPIDocker.bash
@@ -1,4 +1,4 @@
 docker stop $(docker ps -a -q -f name=droneapi)
 docker rm $(docker ps -a -q -f name=droneapi)
-docker build -f APIDockerBuild/Dockerfile -t lesterthomas/droneapi:1.8.2 .
-docker run -p 1235:1234 --link redis:redis -e "DRONEAPI_URL=http://localhost:1235" -e "DOCKER_HOST_IP=172.17.0.1" -e "DOCKER_DRONESIM_IMAGE=lesterthomas/dronesim:1.7" --name droneapi lesterthomas/droneapi:1.8.2 &
+docker build -f APIDockerBuild/Dockerfile -t lesterthomas/droneapi:1.8.4 .
+docker run -p 1235:1234 --link redis:redis -e "DRONEAPI_URL=http://localhost:1235" -e "DOCKER_HOST_IP=172.17.0.1" -e "DOCKER_DRONESIM_IMAGE=lesterthomas/dronesim:1.7" --name droneapi lesterthomas/droneapi:1.8.4 &
