6b22b3d4cba344cdbe6b98320d4401713dc87de0
Lester Thomas <lesterthomas@hotmail.com>
2017-06-28 21:17:51 +0100

    v1.8.11 Added Jenkins trigger to git push script

--- gitPush.bash
+++ gitPush.bash
@@ -13,16 +13,19 @@ VERSION="$MAJOR_VERSION.$MINOR_VERSION"
 echo "VERSION"
 echo $VERSION
 
+echo "Running locally"
+docker stop $(docker ps -a -q -f name=droneapi)
+docker rm $(docker ps -a -q -f name=droneapi)
+docker build -f APIDockerBuild/Dockerfile -t lesterthomas/droneapi:$VERSION .
+docker run -p 1235:1234 -d --link redis:redis -e "DRONEAPI_URL=http://172.17.0.1:1235" -e "DOCKER_HOST_IP=172.17.0.1" -e "DOCKER_DRONESIM_IMAGE=lesterthomas/dronesim:1.7" --name droneapi lesterthomas/droneapi:$VERSION 
+
 echo "Committing to GitHub"
 
 git add .
 git commit -m "v$VERSION $1"
 git push 
 
-echo "Running locally"
-
-docker run -d -p 1235:1234 --link redis:redis -e "DRONEAPI_URL=http://localhost:1235" -e "DOCKER_HOST_IP=172.17.0.1" -e "DOCKER_DRONESIM_IMAGE=lesterthomas/dronesim:1.7" --name droneapi lesterthomas/droneapi:$VERSION 
-sleep 20
+sleep 10
 
 echo "Triggering Postman tests via Jenkins"
 
